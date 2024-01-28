312f8306b37fcd2399cad8812aa1bb2dc41f25fa
Demitri <demitrigilhooley@googlemail.com>
2024-01-28 15:12:07 +0000

    Listfix

--- Assets/Scripts/UI/ControlGroupController.cs
+++ Assets/Scripts/UI/ControlGroupController.cs
@@ -1,43 +1,42 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ControlGroupController : MonoBehaviour
 {
-   [SerializeField] private RectTransform[] _spawnGroups;
-   private List<GameObject> _objects;
+    [SerializeField] private RectTransform[] _spawnGroups;
+    private List<GameObject> _objects = new List<GameObject>(0);
 
-   public void Init(List<GameObject> objs)
-   {
-      _objects = new List<GameObject>();
-      _objects.AddRange(objs);
-      
-      _objects[0].transform.SetParent(_spawnGroups[0]);
-      _objects[1].transform.SetParent(_spawnGroups[1]);
-      _objects[2].transform.SetParent(_spawnGroups[2]);
+    public void Init(List<GameObject> objs)
+    {
+        _objects = new List<GameObject>();
+        _objects.AddRange(objs);
 
-      var r = Random.Range(0, _spawnGroups.Length);
-      
-      for (var i = 2; i < _objects.Count; i++)
-      {
-         _objects[i].transform.SetParent(_spawnGroups[r]);
-      }
+        _objects[0].transform.SetParent(_spawnGroups[0]);
+        _objects[1].transform.SetParent(_spawnGroups[1]);
+        _objects[2].transform.SetParent(_spawnGroups[2]);
 
-      foreach (var obj in _objects)
-      {
-         obj.transform.localScale = Vector3.one;
-      }
-      gameObject.SetActive(true);
-   }
-   
-   public void ClearObjects()
-   {
-      if(_objects.Count == 0) return;
-      foreach (var obj in _objects)
-      {
-         Destroy(obj);
-      }
-      _objects = new List<GameObject>();
-      gameObject.SetActive(false);
-   }
+        var r = Random.Range(0, _spawnGroups.Length);
+
+        for (var i = 2; i < _objects.Count; i++)
+        {
+            _objects[i].transform.SetParent(_spawnGroups[r]);
+        }
+
+        foreach (var obj in _objects)
+        {
+            obj.transform.localScale = Vector3.one;
+        }
+        gameObject.SetActive(true);
+    }
+
+    public void ClearObjects()
+    {
+        if (_objects.Count == 0) return;
+        foreach (var obj in _objects)
+        {
+            Destroy(obj);
+        }
+        _objects = new List<GameObject>();
+        gameObject.SetActive(false);
+    }
 }
